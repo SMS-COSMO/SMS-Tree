c3a46cab93af864a2e512bbd9213cfa53691c0bb
ZTL-UwU <zhangtianli2006@163.com>
2023-12-21 13:46:07 +0800

    feat: add error page

--- error.vue
+++ error.vue
@@ -1,9 +1,16 @@
 <template>
   <el-card class="card mx-auto">
-    <h1 style="font-size: 80px; text-align: center; margin: 20px;">404</h1>
+    <h1 style="font-size: 80px; text-align: center; margin: 20px;">{{ error?.statusCode }}</h1>
     <h1 style="font-size: 30px; text-align: center;">Page Not Found</h1>
   </el-card>
 </template>
+
+<script setup lang="ts">
+defineProps({
+  error: Object
+});
+</script>
+
 
 <style scoped lang="scss">
 .card {
