f8d7f99f508b70eed8ac86d0672880745ba2d351
ZTL-UwU <zhangtianli2006@163.com>
2024-02-27 22:19:34 +0800

    ci: fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/SMS-Tree',
   lang: 'zh-cn',
   lastUpdated: true,
   title: 'SMS-Tree',
