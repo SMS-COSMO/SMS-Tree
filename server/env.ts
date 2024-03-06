7b91de948016c391c017da7d89ea5afe8f963da8
AsyncFox <59872475+AsyncFox@users.noreply.github.com>
2024-03-06 13:55:25 +0000

    chore: allow NODE_ENV to be test

--- server/env.ts
+++ server/env.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   DATABASE_CONNECTION_TYPE: z.enum(['remote', 'local']),
   DATABASE_AUTH_TOKEN: z.string(),
-  NODE_ENV: z.enum(['development', 'production']),
+  NODE_ENV: z.enum(['development', 'production', 'test']),
   SIGN_PUBLIC_KEY: z.string(),
   SIGN_PRIVATE_KEY: z.string(),
   ENC_PUBLIC_KEY: z.string(),
