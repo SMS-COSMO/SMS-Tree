187c567c8df9bf61585cb1bff642d45c39f4f711
ZTL-UwU <zhangtianli2006@163.com>
2024-03-10 11:36:58 +0800

    test: update test db

--- server/db/schema/group.ts
+++ server/db/schema/group.ts
@@ -1,7 +1,7 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { makeId } from '../../trpc/utils/shared';
 import { users } from './user';
 import { classes } from './class';
-import { makeId } from '../../trpc/utils/shared';
 
 export const groups = sqliteTable('groups', {
   id: text('id', { mode: 'text' }).primaryKey().$defaultFn(() => makeId(12)),
