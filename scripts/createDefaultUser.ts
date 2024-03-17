c6a78a76546d7f65c37ef350529fbedc6ff6f04b
AsyncFox <dev.asyncfox@gmail.com>
2024-03-17 12:21:59 +0800

    fix(scripts): use nanoid

--- scripts/createDefaultUser.ts
+++ scripts/createDefaultUser.ts
@@ -1,8 +1,9 @@
+import { nanoid } from 'nanoid';
 import { UserController } from '~/server/trpc/controllers/user';
 import { makeId } from '~/server/trpc/utils/shared';
 
 const userController = new UserController();
-const password = makeId(10);
+const password = nanoid(10);
 await userController.register({ id: 'admin', username: 'admin', role: 'admin', password });
 console.log(`Created default admin user. \nUserID: 'admin' \nPassword: '${password}'`);
 
