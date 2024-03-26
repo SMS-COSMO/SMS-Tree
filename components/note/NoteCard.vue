1fa0e1daf4e62c2ff8f3268baf9a6bc933742932
ZTL-UwU <zhangtianli2006@163.com>
2024-03-26 13:37:14 +0800

    feat(note): add description view

--- components/note/NoteCard.vue
+++ components/note/NoteCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card class="mb-2 cursor-pointer lg:mb-2.5 hover:border-color-[#D4D7DE]! hover:bg-hover-bg!">
+  <el-card class="mb-2 cursor-pointer lg:mb-2.5 hover:border-color-[#D4D7DE]! hover:bg-hover-bg!" @click="dialogVisible = true">
     <el-row class="gap-[6px]">
       <el-tag type="info" disable-transitions>
         {{ note.time.toLocaleDateString('zh-CN') }}
@@ -11,10 +11,91 @@
       </el-text>
     </div>
   </el-card>
+
+  <client-only>
+    <el-dialog
+      v-model="dialogVisible"
+      title="活动记录"
+      width="500"
+    >
+      <el-descriptions
+        :column="1"
+        size="large"
+        border
+      >
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              活动主题
+            </div>
+          </template>
+          <span class="text-[22px] font-bold">
+            {{ note.title }}
+          </span>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              时间
+            </div>
+          </template>
+          {{ note.time.toLocaleDateString('zh-CN') }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              上次活动跟进
+            </div>
+          </template>
+          {{ note.followUp }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              新的讨论内容
+            </div>
+          </template>
+          {{ note.newDiscussion }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              活动笔记
+            </div>
+          </template>
+          {{ note.content }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              下次活动计划
+            </div>
+          </template>
+          {{ note.plans }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="font-bold">
+              反思
+            </div>
+          </template>
+          {{ note.reflections }}
+        </el-descriptions-item>
+      </el-descriptions>
+    </el-dialog>
+  </client-only>
 </template>
 
 <script setup lang="ts">
 import type { TRawNote } from '~/server/db/db';
 
 defineProps<{ note: TRawNote }>();
+
+const dialogVisible = ref(false);
 </script>
+
+<style>
+.el-descriptions__label {
+  width: 200px;
+}
+</style>
