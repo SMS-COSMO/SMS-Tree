6499c432687deaaf9dcfd76cdb5bb04d843b4504
ZTL-UwU <zhangtianli2006@163.com>
2024-05-12 13:17:50 +0800

    feat(admin): improve navbar
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/layouts/AdminNavBar.vue
+++ components/layouts/AdminNavBar.vue
@@ -16,23 +16,40 @@
       <el-icon class="i-tabler:users" />
       学生列表
     </el-menu-item>
-    <el-sub-menu index="1">
+    <el-sub-menu index="0">
       <template #title>
-        <el-icon class="i-tabler:circle-plus" />
-        <span>创建</span>
+        <el-icon class="i-tabler:database" />
+        <span>数据管理</span>
       </template>
-      <el-menu-item index="/admin/user/create">
-        <el-icon class="i-tabler:user-plus" />
-        创建账户
-      </el-menu-item>
-      <el-menu-item index="/admin/paper/create">
-        <el-icon class="i-tabler:file-plus" />
-        创建论文
-      </el-menu-item>
-      <el-menu-item index="/admin/class/create">
-        <el-icon class="i-tabler:school" />
-        创建班级
-      </el-menu-item>
+
+      <el-menu-item-group>
+        <template #title>
+          <span>换届管理</span>
+        </template>
+        <el-menu-item index="/admin/class/delete">
+          <el-icon class="i-tabler:trash-x" />
+          删除过期班级
+        </el-menu-item>
+      </el-menu-item-group>
+
+      <el-menu-item-group>
+        <template #title>
+          <span>创建</span>
+        </template>
+
+        <el-menu-item index="/admin/user/create">
+          <el-icon class="i-tabler:user-plus" />
+          创建账户
+        </el-menu-item>
+        <el-menu-item index="/admin/paper/create">
+          <el-icon class="i-tabler:file-plus" />
+          创建论文
+        </el-menu-item>
+        <el-menu-item index="/admin/class/create">
+          <el-icon class="i-tabler:school" />
+          创建班级
+        </el-menu-item>
+      </el-menu-item-group>
     </el-sub-menu>
   </el-menu>
 </template>
---
+++ pages/admin/class/delete.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-card>
+    <el-alert
+      title="危险操作"
+      type="error"
+      description="操作不可撤销，请注意检查后再操作。"
+      :closable="false"
+    />
+
+    TODO
+  </el-card>
+</template>
