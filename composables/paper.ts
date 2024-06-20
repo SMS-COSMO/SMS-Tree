686b354d6569837d253336b027c789f0c5839d64
Harry Huang <harryhuang2652@qq.com>
2024-06-20 14:32:09 +0800

    fix(paper): optimize score color mapping

--- composables/paper.ts
+++ composables/paper.ts
@@ -1,8 +1,9 @@
 export function useScoreColor(score: TPaperScore) {
-  if (score === 'A')
-    return 'success';
-  else if (score === 'B')
-    return 'warning';
-  else
-    return 'danger';
+  switch (score) {
+    case 'A': return 'success';
+    case 'B': return 'primary';
+    case 'C': return 'warning';
+    case 'D': return 'danger';
+    default: return 'info';
+  }
 }
