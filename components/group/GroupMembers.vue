b31c98cc4fcb79512ceba157d3df61a25508ddaa
Harry Huang <harryhuang2652@qq.com>
2024-06-22 23:19:07 +0800

    feat: add popover of group members

--- components/group/GroupMembers.vue
+++ components/group/GroupMembers.vue
@@ -12,7 +12,21 @@
         :href="`${isAdmin ? '/admin' : ''}/user/${author?.id}`"
         :class="`${showLeader && author?.id === leader?.id ? 'font-bold!' : ''}`"
       >
-        {{ author?.username }}
+        <el-popover placement="top" :width="300" trigger="hover" :show-after="600">
+          <template #reference>
+            {{ author?.username }}
+          </template>
+          <el-descriptions title="组员信息" :column="1" border>
+            <el-descriptions-item label="ID">
+              <el-tag type="info" disable-transitions>
+                {{ author?.id }}
+              </el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="姓名">
+              {{ author?.username }}
+            </el-descriptions-item>
+          </el-descriptions>
+        </el-popover>
       </el-link>
     </span>
   </span>
