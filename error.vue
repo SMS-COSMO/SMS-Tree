f7b7dd86b0475647e89440310fb7a7f7a403eed0
ZTL-UwU <zhangtianli2006@163.com>
2024-07-01 22:01:13 +0800

    chore: improve error page
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- error.vue
+++ error.vue
@@ -1,14 +1,25 @@
 <!-- HTTP Response Code Error Page -->
 
 <template>
-  <el-card class="mx-auto mt-[30vh] max-w-200">
-    <h1 class="m-4 text-center text-20">
-      {{ error?.statusCode }}
-    </h1>
-    <h1 class="text-center text-[30px]">
-      {{ error?.message }}
-    </h1>
-  </el-card>
+  <div class="h-content flex flex-col px-4">
+    <el-card class="mx-auto my-auto max-w-[800px] w-full">
+      <el-result icon="error">
+        <template #title>
+          <h1>
+            {{ errorCode(error?.statusCode) }}
+          </h1>
+        </template>
+        <template #sub-title>
+          <pre class="bg-slate-100 px-5 py-2 text-slate-600 rounded">{{ error?.statusMessage }}</pre>
+        </template>
+        <template #extra>
+          <el-button @click="navigateTo('/')">
+            返回主页
+          </el-button>
+        </template>
+      </el-result>
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,4 +28,16 @@ import type { NuxtError } from '#app';
 defineProps({
   error: Object as () => NuxtError,
 });
+
+function errorCode(code?: number) {
+  code ??= 404;
+  switch (code) {
+    case 404:
+      return '404 页面不存在';
+    case 500:
+      return '500 服务器内部错误';
+    default:
+      return code.toString();
+  }
+};
 </script>
