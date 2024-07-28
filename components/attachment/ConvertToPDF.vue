ca8382975a19a72b5a9c7fd49ee225029fda96a6
ZTL-UwU <zhangtianli2006@163.com>
2024-07-28 21:31:56 +0800

    feat: use el-image for convert explainer
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/attachment/ConvertToPDF.vue
+++ components/attachment/ConvertToPDF.vue
@@ -8,62 +8,117 @@
   >
     <el-tabs v-model="activeName">
       <el-tab-pane label="Word 文档" name="docx">
-        <h2>WPS</h2>
-        <el-steps direction="vertical" :active="-1">
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-to-pdf1.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-to-pdf2.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-to-pdf3.png" class="max-w-full" />
-            </template>
-          </el-step>
-        </el-steps>
+        <div class="grid grid-cols-2">
+          <div class="pb-20">
+            <h2>WPS</h2>
+            <el-steps direction="vertical" :active="-1">
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-to-pdf1.png"
+                    :preview-src-list="wpsToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-to-pdf2.png"
+                    :preview-src-list="wpsToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-to-pdf3.png"
+                    :preview-src-list="wpsToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+            </el-steps>
+          </div>
+        </div>
       </el-tab-pane>
       <el-tab-pane label="PPT" name="ppt">
-        <h2>PowerPoint</h2>
-        <el-steps direction="vertical" :active="-1">
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/ppt-to-pdf1.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/ppt-to-pdf2.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/ppt-to-pdf3.png" class="max-w-full" />
-            </template>
-          </el-step>
-        </el-steps>
-        <h2>WPS</h2>
-        <el-steps direction="vertical" :active="-1">
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-ppt-to-pdf1.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-ppt-to-pdf2.png" class="max-w-full" />
-            </template>
-          </el-step>
-          <el-step>
-            <template #description>
-              <NuxtImg src="/convertToPDF/wps-ppt-to-pdf3.png" class="max-w-full" />
-            </template>
-          </el-step>
-        </el-steps>
+        <div class="grid md:grid-cols-2">
+          <div class="pb-20">
+            <h2>PowerPoint</h2>
+            <el-steps direction="vertical" :active="-1">
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/ppt-to-pdf1.png"
+                    :preview-src-list="pptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/ppt-to-pdf2.png"
+                    :preview-src-list="pptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/ppt-to-pdf3.png"
+                    :preview-src-list="pptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+            </el-steps>
+          </div>
+          <div class="pb-20">
+            <h2>WPS</h2>
+            <el-steps direction="vertical" :active="-1">
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-ppt-to-pdf1.png"
+                    :preview-src-list="WpsPptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-ppt-to-pdf2.png"
+                    :preview-src-list="WpsPptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+              <el-step>
+                <template #description>
+                  <el-image
+                    class="h-40 w-40 shadow transition-all rounded hover:shadow-md"
+                    src="/convertToPDF/wps-ppt-to-pdf3.png"
+                    :preview-src-list="WpsPptToPDF"
+                    fit="cover"
+                  />
+                </template>
+              </el-step>
+            </el-steps>
+          </div>
+        </div>
       </el-tab-pane>
     </el-tabs>
 
@@ -78,4 +133,8 @@
 <script lang="ts" setup>
 const dialogVisible = ref(false);
 const activeName = ref('docx');
+
+const wpsToPDF = ['/convertToPDF/wps-to-pdf1.png', '/convertToPDF/wps-to-pdf2.png', '/convertToPDF/wps-to-pdf3.png'];
+const pptToPDF = ['/convertToPDF/ppt-to-pdf1.png', '/convertToPDF/ppt-to-pdf2.png', '/convertToPDF/ppt-to-pdf3.png'];
+const WpsPptToPDF = ['/convertToPDF/wps-ppt-to-pdf1.png', '/convertToPDF/wps-ppt-to-pdf2.png', '/convertToPDF/wps-ppt-to-pdf3.png'];
 </script>
